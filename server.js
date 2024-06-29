58393d3c5728def09f908bf9870304618a23d613
slee1996 <spencerlee96@gmail.com>
2024-06-28 19:59:29 -0600

    man

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const https = require("https");
 const bodyParser = require("body-parser");
 
 const { generateRawLyrics } = require("./lib/lyric-generation");
@@ -53,6 +54,6 @@ app.get("/", async (req, res) => {
   res.send("hello world");
 });
 
-app.listen(PORT, () => {
+https.createServer(app).listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
